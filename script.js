b11968d6096fc297fae6cace8bfef6193bf50434
abhijitv <calljeet_2001@yahoo.com>
2019-05-09 09:59:44 +0800

    starting the core  drawing script

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas= document.getElementById('#canvas');
+const ctx = canvas.getcContext('2d');
+const penColor =document.querySelector('input [name="penColor"]');
+const penWidth = document.querySelector('input [name="penWidth"]');
+const Saver = document.querySelector('#saver');
