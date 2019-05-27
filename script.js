5fd36310fe9073d8690811c3744ad6f2564910f6
abhijitv <calljeet_2001@yahoo.com>
2019-05-27 20:35:01 +0800

    draw a rectangle

--- script.js
+++ script.js
@@ -3,32 +3,31 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 const penColor = document.querySelector('input[name="penColor"]');
 const penWidth = document.querySelector('input[name="penWidth"]');
-const Saver = document.querySelector('#saver');
+const Saver = document.querySelector("#saver");
 
-ctx.strokeStyle = '#000000';
-ctx.lineJoin = 'round';
-ctx.linCap = 'round';
+ctx.strokeStyle = "#000000";
+ctx.lineJoin = "round";
+ctx.linCap = "round";
 ctx.lineWidth = 5;
 
 let pen = {
   x: 0,
   y: 0,
-  down: false,
+  down: false
 };
 
-
 function saveFile() {
-  console.log('saving file');
+  console.log("saving file");
   let image = canvas.toDataURL();
-  document.getElementById('myImage').src = image;
+  document.getElementById("myImage").src = image;
 }
 
 function draw(e) {
-  console.log('move');
+  console.log("move");
   if (!pen.down) return;
   ctx.lineWidth = penWidth.value;
   ctx.strokeStyle = penColor.value;
@@ -46,12 +45,12 @@ function penDown(e) {
 }
 
 function noDown() {
-  console.log('out');
+  console.log("out");
   pen.down = false;
 }
 
-Saver.addEventListener('click', saveFile);
-canvas.addEventListener('mousedown', penDown);
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', noDown);
-canvas.addEventListener('mouseout', noDown);
+Saver.addEventListener("click", saveFile);
+canvas.addEventListener("mousedown", penDown);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", noDown);
+canvas.addEventListener("mouseout", noDown);
